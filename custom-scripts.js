ea20d30829a7cc79e8377ffdbdffec5fa03a4da1
Callum <callum.git@outlook.com>
2019-09-21 12:47:50 +0100

    Footer removed from landing page

--- custom-scripts.js
+++ custom-scripts.js
@@ -14,3 +14,6 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// var form = document.getElementById('contact-en');
+// var isValidForm = form.checkValidity();
